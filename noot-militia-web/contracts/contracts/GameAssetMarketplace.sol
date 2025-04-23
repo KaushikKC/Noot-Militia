52756520bec462c3862b1a6bcc6fd90f4237b93f
Ashwin-3cS <flashashwin2004@gmail.com>
2025-04-24 02:13:52 +0530

    tested MarketPlace contract

--- noot-militia-web/contracts/contracts/GameAssetMarketplace.sol
+++ noot-militia-web/contracts/contracts/GameAssetMarketplace.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-// Removed Counters import
+//tested 
 
 /**
  * @title GameAssetMarketplace
